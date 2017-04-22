17d62e0ea3e6680bd58e91a27ffb7f5dcedca315
Anton Smetanin <AntonSmetanin@gmail.com>
2017-04-22 16:35:37 +0300

    basic controls

---
+++ Assets/Scripts/Controls/KeyControls.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class KeyControls : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3f;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector2(Input.GetAxis("Vertical"), -Input.GetAxis("Horizontal")) * _speed, Space.World);
+    }
+}
---
+++ Assets/Scripts/Controls/MouseControls.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class MouseControls : MonoBehaviour
+{
+    [SerializeField] private float _speed = 3f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), -Input.GetAxis("Mouse X"), 0) * _speed, Space.World);
+        }
+    }
+}
