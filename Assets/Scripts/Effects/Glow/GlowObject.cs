745ed14ac3822c1c6c857f8bc3180d56d570da94
Zakharov Aleksandr <sharky.pro@mail.ru>
2017-04-23 18:07:45 +0300

    Atmosphere.

---
+++ Assets/Scripts/Effects/Glow/GlowObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class GlowObject : MonoBehaviour
+{
+	public Color GlowColor;
+	public Renderer[] Renderers { get; private set; }
+    private List<Material> _materials = new List<Material>();
+	
+	void Start()
+	{
+		Renderers = GetComponentsInChildren<Renderer>();
+
+		foreach (var renderer in Renderers)
+			_materials.AddRange(renderer.materials);
+	}
+    
+    private void Update()
+	{
+		for (int i = 0; i < _materials.Count; i++)
+			_materials[i].SetColor("_GlowColor", GlowColor);
+	}
+}
