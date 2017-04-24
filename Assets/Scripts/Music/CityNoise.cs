b798c857d34ce18381689e3b12fa6088b825bb99
Aleksandr Zakharov <sharky.pro@mail.ru>
2017-04-25 00:56:09 +0300

    Music. City Noise.

---
+++ Assets/Scripts/Music/CityNoise.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class CityNoise : MonoBehaviour
+{
+    [SerializeField] private AudioSource _noiseAudioSource;
+    [SerializeField] private AnimationCurve _noiseCurve;
+
+	private void Update ()
+	{
+	    _noiseAudioSource.volume = 0.2f + _noiseCurve.Evaluate(Time.time) * Mathf.Clamp(ParametersCounter.GetPopulationSum() / 750, 0, 1);
+	}
+}
---
+++ Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip[] Tracks;
+    public float FadeTime = 1.0f;
+    private int _currentTrack = 0;
+    private bool _isSwitched = false;
+    private AudioSource _currentAudioSource;
+	
+	private void Awake ()
+	{
+	    Crossfade(Tracks[GetTrackNumber()]);
+	}
+	
+	private void Update ()
+    {
+        if (_currentAudioSource != null && _currentAudioSource.clip != null && _currentAudioSource.clip.length - _currentAudioSource.time < 30 && !_isSwitched)
+        {
+            _isSwitched = true;
+            Crossfade(Tracks[GetTrackNumber()]);
+        }
+	}
+
+    public void Crossfade(AudioClip newTrack)
+	{
+		StopAllCoroutines();
+
+		if(GetComponents<AudioSource>().Length > 1)
+		{
+			Destroy(GetComponent<AudioSource>());
+		}
+
+        _currentAudioSource = gameObject.AddComponent<AudioSource>();
+        _currentAudioSource.volume = 0.0f;
+        _currentAudioSource.clip = newTrack;
+        _currentAudioSource.Play();
+        StartCoroutine(ActuallyCrossfade(_currentAudioSource));
+	}
+
+	IEnumerator ActuallyCrossfade(AudioSource newSource)
+	{
+		float t = 0.0f;
+        float initialVolume = GetComponent<AudioSource>().volume;
+
+		while(t < FadeTime)
+		{
+			GetComponent<AudioSource>().volume = Mathf.Lerp(initialVolume,0.0f,t/FadeTime);
+			newSource.volume = Mathf.Lerp(0.0f,1.0f,t/FadeTime);
+
+			t += Time.deltaTime;
+			yield return null;
+		}
+
+		newSource.volume = 1.0f;
+	    _isSwitched = false;
+        Destroy(GetComponent<AudioSource>());
+	}
+
+    private int GetTrackNumber()
+    {
+        var number = _currentTrack;
+        if (_currentTrack >= Tracks.Length)
+        {
+            _currentTrack = 0;
+            number = _currentTrack;
+        }
+        _currentTrack++;
+        Debug.Log(number);
+        return number;
+    }
+}
