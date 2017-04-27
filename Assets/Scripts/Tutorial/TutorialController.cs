c41a1abae0964f773ee0c2685723fe342fa34942
pauld <paul.dyatlov@gmail.com>
2017-04-27 20:36:42 +0400

    some tutorial wip

---
+++ Assets/Scripts/Tutorial/TutorialController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    [SerializeField] private List<TutorialScenarioPanel> _tutorialScenarios;
+
+    public void Init(Action onAllTutorialPassed)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Tutorial/TutorialScenarioPanel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialScenarioPanel : MonoBehaviour
+{
+    [SerializeField] private Text _descriptionLabel;
+    [SerializeField] private Button _acceptButton;
+
+    private Action _onProceed;
+
+    private void Awake()
+    {
+        _acceptButton.onClick.AddListener(() =>
+        {
+            _onProceed();
+        });
+    }
+
+    public void Show(Action onProceed)
+    {
+        gameObject.SetActive(true);
+
+        _onProceed = onProceed;
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
